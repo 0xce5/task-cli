3db82d7eff5d382e98cda44875ccddbc159bc304
oxce5_root <avg.gamer@proton.me>
2025-03-15 00:17:52 +0800

    add: main() and relevant functions

---
+++ task-cli.py
@@ -0,0 +1,42 @@
+import sys
+
+operator = sys.argv[1]
+argument = sys.argv[2] if len(sys.argv) > 2 else None
+comment = sys.argv[3] if len(sys.argv) > 2 else None
+
+def add(argument):
+        pass
+
+def delete(argument):
+        pass
+
+
+def update(argument):
+        pass
+
+def mark(argument):
+        pass
+
+def list(argument):
+    pass
+
+def main():
+    match operator:
+        case "add":
+            add(argument)
+        case "update":
+            pass
+        case "delete":
+            pass
+        case "mark ":
+            add(argument)
+        case "list":
+            pass
+        case _:
+            print("Invalid operator!")
+            
+
+
+if __name__ == '__main__':
+  main()
+
