600f3647e7aa4ef8ca427f80b11ed1ac37585fef
oxce5_root <avg.gamer@proton.me>
2025-03-15 19:52:59 +0800

    add: json template

--- task-cli.py
+++ task-cli.py
@@ -1,9 +1,24 @@
 import sys
 import json
+from datetime import date
 
 operator = sys.argv[1]
 argument = sys.argv[2] if len(sys.argv) > 2 else None
 comment = sys.argv[3] if len(sys.argv) > 3 else None
+
+id = None
+desc = None
+status = None
+created = date.today()
+updated = None
+
+template = {
+    'id': id,
+    'desc': desc,
+    'status': status,
+    'createdAt': created,
+    'updatedAt': updated
+}
 
 def getJson():
     pass
